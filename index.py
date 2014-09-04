e5f4a337f3f6a1b14474a44ec0c438020d5b5cca
AndrejHronco <andrejhronco@gmail.com>
2014-09-03 18:05:54 -0700

    Added structure markup and audio files
    basic html5 structure
    audio element
    audio files (mp3, ogg)

--- index.py
+++ index.py
@@ -1,5 +1,35 @@
 #!/usr/bin/env python
 
-print "Content-Type: text/html\n\n"
-print "Hello, Andrej"
-print "Niiiice!"
+print """
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Gamelan Note Tester</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!--<link rel="stylesheet" href="css/normalize.min.css">-->
+        <!--<link rel="stylesheet" href="css/main.css">-->
+
+        <!--<script src="js/vendor/modernizr-2.6.2.min.js"></script>-->
+    </head>
+    <body>
+"""
+
+print """
+<div id='container'>
+<audio src="audio/calung.ogg" controls autoplay loop>
+<p>Your browser does not support the audio element </p>
+</audio>
+</div>
+"""
+
+print """
+    </body>
+</html>
+"""
