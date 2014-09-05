82155d73627da10ae542eab7cd70e455ed519ba5
AndrejHronco <andrejhronco@gmail.com>
2014-09-04 18:06:34 -0700

    Updated structure and added logic for the quiz
    imported os, sys
    access form data
    parsing form data for comparison

--- index.py
+++ index.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import os
 import sys
 import random
+import cgi
+import cgitb
+cgitb.enable()
 
 print "Content-Type: text/html"
 print
-print """
-<!DOCTYPE html>
+print """<!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
@@ -35,8 +39,8 @@ print "<div id='container'>"
 #  audio player
 print """
 <audio src="audio/{0}.mp3" controls autoplay loop>
-	<source src="audio/{0}.mp3" type="audio/mp3">
- 	<source src="audio/{0}.ogg" type="audio/ogg">
+  <source src="audio/{0}.mp3" type="audio/mp3">
+  <source src="audio/{0}.ogg" type="audio/ogg">
 <p>Your browser does not support the audio element </p>
 </audio>""".format(note)
 
@@ -45,24 +49,32 @@ print"""
 <h3>Select which note just played and click the submit button.</h3>
 
 <form action="" method="POST">
-	<input type="hidden" name="answer" value="{0}">
-	<label for="ding">Ding</label>
-  	<input type="radio" name="note" id="ding" value="ding"><br>
-	<label for="dong">Dong</label>
-  	<input type="radio" name="note" id="dong" value="dong"><br>
-	<label for="deng">Deng</label>
-  	<input type="radio" name="note" id="deng" value="deng"><br>
-	<label for="dung">Dung</label>
-  	<input type="radio" name="note" id="dung" value="dung"><br>
-	<label for="dang">Dang</label>
-  	<input type="radio" name="note" id="dang" value="dang"><br>
-  	<input type="submit" value='Submit Answer'>
+  <input type="hidden" name="answer" value="{0}">
+  """
+for n in notes:
+  print '''<label for="{0}">{1}</label>
+  <input type="radio" name="{0}" id="{0}" value="{0}"><br>'''.format(n, n.capitalize())
+
+print """<input type="submit" value='Submit Answer'>
 </form>""".format(note)
 
 print "</div>"
 
-print os.getenv("QUERY_STRING")
-#sys.stdin.read()
+def print_list_html(l):
+  for s in l:
+    print cgi.escape(s)
+    print '<br>'
+
+#print "Content Length:", os.getenv("CONTENT_LENGTH")
+cl = os.getenv("CONTENT_LENGTH")
+if cl:
+  data = sys.stdin.read(int(cl))
+  parts = data.split("&")
+
+  for bits in parts:
+    kv = bits.split("=")
+    print "value: " + kv[1] + "<br>"
+
 
 print """
     </body>
